9e4f618b27079b439061e27f7d6696638c9f3f35
깃Boom0704 <relonging@naver.com>
2023-03-27 10:14:38 +0900

    first App

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,24 @@
+package org.example;
+import java.util.Scanner;
+
+public class App {
+    private final Scanner sc;
+
+    public App(Scanner sc) {
+        this.sc = sc;
+    }
+
+    public void run() {
+        System.out.println("== 명언 앱 ==");
+
+        while (true) {
+            System.out.print("명령) ");
+            // trim() : 혹시 있을지 모를 좌우공백제거된 버전으로 주세요.
+            String command = sc.nextLine().trim();
+
+            if (command.equals("종료")) {
+                break;
+            }
+        }
+    }
+}
